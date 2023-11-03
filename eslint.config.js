ed45e7fd9985ab9dc1f058b6379cbb1819e2da24
productdevbook <hi@productdevbook.com>
2023-11-03 18:53:07 +0300

    fix: lint and upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,12 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu()
+export default antfu(
+  {},
+  {
+    ignores: [
+      'dist',
+      '.github',
+      'node_modules',
+    ],
+  },
+)
